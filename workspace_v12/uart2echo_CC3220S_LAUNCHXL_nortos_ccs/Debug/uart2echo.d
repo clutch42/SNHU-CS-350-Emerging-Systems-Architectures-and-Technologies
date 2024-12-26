c43d334c2dca669f822409980f537a538583af02
Brian Engel <clutch_42@yahoo.com>
2024-12-26 10:48:33 -0600

    added from desktop

---
+++ workspace_v12/uart2echo_CC3220S_LAUNCHXL_nortos_ccs/Debug/uart2echo.d
@@ -0,0 +1,66 @@
+# FIXED
+
+uart2echo.obj: ../uart2echo.c
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/GPIO.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/UART2.h
+uart2echo.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/dpl/ClockP.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/dpl/HwiP.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/dpl/SemaphoreP.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/utils/RingBuf.h
+uart2echo.obj: syscfg/ti_drivers_config.h
+uart2echo.obj: C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/devices/DeviceFamily.h
+
+../uart2echo.c:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/GPIO.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/UART2.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/dpl/ClockP.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/dpl/HwiP.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/dpl/SemaphoreP.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/drivers/utils/RingBuf.h:
+
+syscfg/ti_drivers_config.h:
+
+C:/ti/simplelink_cc32xx_sdk_4_30_00_06/source/ti/devices/DeviceFamily.h:
+
